cf2f322090142a8e13b6a99b0cffb4fa5902794c
Sibi <psibi2000@gmail.com>
2012-05-31 13:51:53 +0530

    Constants

---
+++ uclassify/uclassify_endpoints.py
@@ -0,0 +1,7 @@
+uclassify_http_status_codes = {
+    2000: ('OK','Success'),
+    4000: ('BAD_REQUEST','The request was invalid'),
+    4013: ('REQUEST_ENTITY_TOO_LARGE','The request data payload is high'),
+    5000: ('INTERNAL_SERVER_ERROR','Problem with uClassify Servers. Check back after some time.'),
+    5030: ('SERVICE_UNAVAILABLE','Service currently unavailble. Try back later.')
+}
