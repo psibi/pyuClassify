fe27f6ab13955900ef76e793806ff5b8160effe6
Sibi <psibi2000@gmail.com>
2012-06-04 07:48:56 +0530

    Setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+__author__ = 'Sibi <sibi@psibi.in>'
+__version__ = '0.1.0'  #major.minor.micro
+
+setup(
+    # Basic package information.
+    name='uclassify',
+    version=__version__,
+    packages=find_packages(),
+
+    # Packaging options.
+    include_package_data=True,
+
+    # Package dependencies.
+    install_requires=['requests'],
+
+    # Metadata for PyPI.
+    author='Sibi',
+    author_email='sibi@psibi.in',
+    license='GNU GPLv3',
+    url='https://github.com/psibi/pyuClassify',
+    keywords='uClassify API',
+    description='Python Wrapper for accessing uClassify services',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ]
+)
